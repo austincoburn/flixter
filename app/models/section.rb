aaf175aaf8cb005922f81a64b3a44aa344edf83c
Austin Coburn <austincoburn41@gmail.com>
2019-09-17 02:10:01 +0000

    Going back to lesson 32

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,18 +1,15 @@
 class Section < ApplicationRecord
-    belongs_to :course
-    has_many :lessons
+  belongs_to :course
+  has_many :lessons
 
-    include RankedModel
-    ranks :row_order, with_same: :course_id
+  validates :title, presence: true
 
-    def next_section
-        section = course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
-        return section
-    end 
+  include RankedModel
+  ranks :row_order, with_same: :course_id
 
-    def previous_section
-        section = course.sections.where("row_order > ?", self.row_order).rank(:row_order).last
-        return section
-    end 
+  def next_section
+    section = course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
+    return section
+  end
 
 end
